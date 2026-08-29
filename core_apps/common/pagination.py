bdcf98a24b508dd2d24e86b49bd871bce57b5596
demo-23home <zeyadslama23@gmail.com>
2026-08-29 20:08:38 +0300

    feat: update default page size in StandardResultsSetPagination to 20

--- core_apps/common/pagination.py
+++ core_apps/common/pagination.py
@@ -7,6 +7,6 @@ class StandardResultsSetPagination(PageNumberPagination):
     Allows query parameter-driven page sizing with a maximum ceiling.
     """
 
-    page_size = 9
+    page_size = 20
     page_size_query_param = "page_size"
     max_page_size = 100
